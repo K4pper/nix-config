fd10415a6bcec61b915360bd6f6d5cab53a4fb04
Kasper Therkelsen <kasper@kaspertherkelsen.com>
2024-07-13 22:32:20 +0200

    flake: added home-manager config

--- hosts/nixos/flake.nix
+++ hosts/nixos/flake.nix
@@ -5,18 +5,28 @@
   inputs = {
     # Nix knows where nixpkgs are, therefore the full github URI is not needed
     nixpkgs.url = "nixpkgs/nixos-24.05";
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, ... }: 
+  outputs = { self, nixpkgs, home-manager, ... }: 
   let
     lib = nixpkgs.lib;
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
   in {
     nixosConfigurations = {
       nixos = lib.nixosSystem {
-        system = "x86-64-linux";
-	modules = [ ./configuration.nix ];
+        inherit system;
+	      modules = [ ./configuration.nix ];
       };
     };
+    homeConfigurations = {
+        kapper = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [ ./home.nix ];
+        };
+      };
   };
 
 }
