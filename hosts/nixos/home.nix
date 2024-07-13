c4cde137faad0bc35047bc44b10a3bcd5f7a07fa
Kasper Therkelsen <kasper@kaspertherkelsen.com>
2024-07-13 22:45:21 +0200

    home-manager: added zsh

--- hosts/nixos/home.nix
+++ hosts/nixos/home.nix
@@ -73,4 +73,11 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+
+  programs.zsh = {
+    enable = true;
+    shellAliases = {
+      vim = "nvim";
+    }
+  };
 }
