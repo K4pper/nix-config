c1543a2b13f30267e6470aa1eed6891f3ccbdaf0
Kasper Therkelsen <kasper@kaspertherkelsen.com>
2024-07-14 17:02:07 +0200

    zsh unsetopt beep

--- modules/nixos/cli/zsh/default.nix
+++ modules/nixos/cli/zsh/default.nix
@@ -8,5 +8,6 @@
     enableCompletion = true;
     dotDir = ".config/zsh";
     completionInit = "autoload -Uz compinit && zstyle ':completion:*' menu select && zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}' && zmodload zsh/complist && compinit";
+    initExtra = "unsetopt BEEP";
   };
 }
