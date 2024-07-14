d27e2cc056c1648cd66aa4adeea06bd2430256be
Kasper Therkelsen <kasper@kaspertherkelsen.com>
2024-07-14 20:45:53 +0200

    nvim opts syntax fix

--- modules/nixos/cli/nvim/settings.nix
+++ modules/nixos/cli/nvim/settings.nix
@@ -1,19 +1,21 @@
 {
-  opts = {
-    number = true;
-    relativenumber = true;
-    cursorline = true;
-    expandtab = true;
-    foldlevel = "99";
-    mouse = "a";
-    clipboard = "unnamedplus";
-    smartindent = true;
-    smartcase = true;
-    spelling = "en";
-    ignorecase = true;
-    linebreak = true;
-    tabstop = "2";
-    termguicolors = true;
-    list = true;
+  config = {
+    opts = {
+      number = true;
+      relativenumber = true;
+      cursorline = true;
+      expandtab = true;
+      foldlevel = "99";
+      mouse = "a";
+      clipboard = "unnamedplus";
+      smartindent = true;
+      smartcase = true;
+      spelling = "en";
+      ignorecase = true;
+      linebreak = true;
+      tabstop = "2";
+      termguicolors = true;
+      list = true;
+    };
   };
 }
