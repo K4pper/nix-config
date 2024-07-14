9c7208a2a29b16c9111b9236c2f9c38119242e50
Kasper Therkelsen <kasper@kaspertherkelsen.com>
2024-07-14 20:51:28 +0200

    testing

--- modules/nixos/cli/nvim/default.nix
+++ modules/nixos/cli/nvim/default.nix
@@ -1,14 +1,14 @@
 {...}:
 {
-  imports = [
-    # General Config
-    ./settings.nix
-  ];
   programs.nixvim = {
     enable = true;
     defaultEditor = true;
     vimdiffAlias = true;
     viAlias = true;
     vimAlias = true;
+  imports = [
+    # General Config
+    ./settings.nix
+    ];
   };
 }
