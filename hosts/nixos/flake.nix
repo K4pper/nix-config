099e5f1bfb12a3eed817bac50a2d0194c3ab9e62
Kasper Therkelsen <kasper@kaspertherkelsen.com>
2024-07-14 12:03:13 +0200

    added nixvim

--- hosts/nixos/flake.nix
+++ hosts/nixos/flake.nix
@@ -7,6 +7,8 @@
     nixpkgs.url = "nixpkgs/nixos-24.05";
     home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nixvim.url = "github:nix-community/nixvim/nixos-24.05";
+    nixvim.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, home-manager, ... }: 
