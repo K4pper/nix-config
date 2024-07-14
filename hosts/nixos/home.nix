cf44a7772b80e8fc3ea8ab521fed67221e5fa79f
Kasper Therkelsen <kasper@kaspertherkelsen.com>
2024-07-14 09:46:22 +0200

    trying more

--- hosts/nixos/home.nix
+++ hosts/nixos/home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, lib, nixvim, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   # Home Manager needs a bit of information about you and the paths it should
@@ -145,6 +145,7 @@
   };
 
   programs.nixvim = {
+    enable = true;
     defaultEditor = true;
   };
 }
