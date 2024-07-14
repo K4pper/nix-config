fdbbd0fa79a1e004ecc0badb01772b61b1bb12e8
Kasper Therkelsen <kasper@kaspertherkelsen.com>
2024-07-14 20:43:35 +0200

    added git config

---
+++ modules/nixos/cli/nvim/settings.nix
@@ -0,0 +1,19 @@
+{
+  opts = {
+    number = true;
+    relativenumber = true;
+    cursorline = true;
+    expandtab = true;
+    foldlevel = "99";
+    mouse = "a";
+    clipboard = "unnamedplus";
+    smartindent = true;
+    smartcase = true;
+    spelling = "en";
+    ignorecase = true;
+    linebreak = true;
+    tabstop = "2";
+    termguicolors = true;
+    list = true;
+  };
+}
