aaf8e6869ff41ac146af320f2a4c46bc52ea649c
Kasper Therkelsen <kasper@kaspertherkelsen.com>
2024-07-14 16:55:59 +0200

    reordering

--- modules/nixos/cli/zsh/default.nix
+++ modules/nixos/cli/zsh/default.nix
@@ -7,6 +7,6 @@
     syntaxHighlighting.enable = true;
     enableCompletion = true;
     dotDir = ".config/zsh";
-    completionInit = "autoload -U compinit && compinit && zstyle ':completion:*' menu select && zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'";
+    completionInit = "autoload -Uz compinit && zstyle ':completion:*' menu select && zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z} && compinit'";
   };
 }
