0b5a07c461c25d7e8e8b38c4fcc656601cbccc42
kapper <kasper@kaspertherkelsen.com>
2024-07-14 09:12:18 +0200

    zsh: added sha256

--- hosts/nixos/home.nix
+++ hosts/nixos/home.nix
@@ -87,6 +87,7 @@
           owner = "zsh-users";
           repo = "zsh-autosuggestions";
           rev = "v0.7.0";
+	  sha256 = "KLUYpUu4DHRumQZ3w59m9aTW6TBKMCXl2UcKi4uMd7w=";
         };
       }
     ];
