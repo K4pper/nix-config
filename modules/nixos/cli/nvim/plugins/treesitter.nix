6c95f4f8e921dbb933f16be8484377b8a9b0801c
Kasper Therkelsen <kasper@kaspertherkelsen.com>
2024-07-15 15:12:48 +0200

    test

--- modules/nixos/cli/nvim/plugins/treesitter.nix
+++ modules/nixos/cli/nvim/plugins/treesitter.nix
@@ -4,10 +4,6 @@
       enable = true;
       nixvimInjections = true;
       folding = true;
-      settings = {
-        highlight.enable = true;
-        indent.enable = true;
-      };
     };
   };
 }
