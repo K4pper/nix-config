136d5a12f860df7d49ac7fcf4968799142b6bf6b
Kasper Therkelsen <kasper@kaspertherkelsen.com>
2024-07-15 15:50:09 +0200

    hide tab bar wezterm

--- modules/nixos/apps/wezterm/default.nix
+++ modules/nixos/apps/wezterm/default.nix
@@ -9,6 +9,7 @@
         font = wezterm.font("JetBrains Mono"),
         font_size = 10.0,
         color_scheme = "Tokyo Night Storm",
+        hide_tab_bar_if_only_one_tab = true,
       }
     '';
   };
