2ae5c2956fa7f28622dbf43af35b05b204260032
Kasper Therkelsen <kasper@kaspertherkelsen.com>
2024-07-20 10:20:18 +0200

    disable splash rendering

--- modules/nixos/desktop/hyprland/default.nix
+++ modules/nixos/desktop/hyprland/default.nix
@@ -59,6 +59,7 @@
     misc = {
       force_default_wallpaper = 1;
       disable_hyprland_logo = false;
+      disable_splash_rendering = true;
       mouse_move_enables_dpms = false;
       key_press_enables_dpms = true;
     };
