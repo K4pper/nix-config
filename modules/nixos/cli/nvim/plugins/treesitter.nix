1070bd77b4610d76075d304910a32cd12e0170ee
Kasper Therkelsen <kasper@kaspertherkelsen.com>
2024-11-27 20:46:29 +0100

    treesitter

--- modules/nixos/cli/nvim/plugins/treesitter.nix
+++ modules/nixos/cli/nvim/plugins/treesitter.nix
@@ -4,6 +4,15 @@
       enable = true;
       nixvimInjections = true;
       folding = true;
+      settings = {
+        highlight = {
+          additional_vim_regex_highlighting = true;
+          enable = true;
+        };
+        indent = {
+          enable = true;
+        };
+      };
     };
   };
 }
