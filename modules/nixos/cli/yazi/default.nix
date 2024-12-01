978bc825ff4e67422012c88ae6280faec83fb7a6
Kasper Therkelsen <kasper@kaspertherkelsen.com>
2024-12-01 09:32:00 +0100

    added todo comment and yazi rose-pine config

--- modules/nixos/cli/yazi/default.nix
+++ modules/nixos/cli/yazi/default.nix
@@ -1,12 +1,17 @@
+# rose-pine is used multiple times, make this use variables instead
 _:
 let
   theme = builtins.readFile ./theme.toml;
 in
 {
-  home.file.".config/yazi/flavors/tokyo-night.yazi/flavor.toml" = {
-    text = builtins.readFile ./flavors/tokyo-night.yazi/flavor.toml;
+  home.file.".config/yazi/flavors/rose-pine.yazi/flavor.toml" = {
+    text = builtins.readFile ./flavors/rose-pine.yazi/flavor.toml;
     executable = false;
   };
+  home.file.".config/yazi/rose-pine.tmTheme" = {
+    text = builtins.readFile ./flavors/rose-pine.yazi/rose-pine.tmTheme;
+    executable = false;
+    };
   programs.yazi = {
     enable = true;
     theme = builtins.fromTOML theme;
