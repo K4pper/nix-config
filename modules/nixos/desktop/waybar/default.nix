10cccbe5a7f1a43de1297e854c8a68907b4a549f
Kasper Therkelsen <kasper@kaspertherkelsen.com>
2024-12-18 20:44:11 +0100

    update and made my waybar ugly

--- modules/nixos/desktop/waybar/default.nix
+++ modules/nixos/desktop/waybar/default.nix
@@ -3,6 +3,10 @@ let
   settings = builtins.readFile ./config.jsonc;
 in
 {
+  home.file.".config/waybar/rose-pine.css" = {
+    text = builtins.readFile ./rose-pine.css;
+    executable = false;
+  };
   programs.waybar = {
     enable = true;
     settings = builtins.fromJSON settings;
