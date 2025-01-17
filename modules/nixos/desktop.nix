f342a40c0be7a6347610b034f4c24fc13833fbd9
Kasper Therkelsen <kasper@kaspertherkelsen.com>
2025-01-17 21:28:49 +0100

    uwsm

--- modules/nixos/desktop.nix
+++ modules/nixos/desktop.nix
@@ -22,7 +22,16 @@
 
 
         # Hyprland & UWSM
-        programs.uwsm.enable = true;
+        programs.uwsm = {
+            enable = true;
+            waylandCompositors = [
+                hyprland = {
+                    prettyName = "Hyprland";
+                    comment = "Hyprland compositor managed by UWSM";
+                    binPath = "/run/current-system/sw/bin/Hyprland";
+                };
+            ];
+        };
         programs.hyprland = {
             withUWSM = true;
             xwayland.enable = true;
