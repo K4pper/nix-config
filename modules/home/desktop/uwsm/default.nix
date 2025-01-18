ee238989351b606d7e08aa325ee67fe8ac0714cf
Kasper Therkelsen <kasper@kaspertherkelsen.com>
2025-01-18 13:57:45 +0100

    more uwsm

--- modules/home/desktop/uwsm/default.nix
+++ modules/home/desktop/uwsm/default.nix
@@ -4,4 +4,8 @@ _:
         text = builtins.readFile ./env;
         executable = false;
     };
+    home.file.".config/uwsm/env-hyprland" = {
+        text = builtins.readFile ./env-hyprland;
+        executable = false;
+    };
 }
