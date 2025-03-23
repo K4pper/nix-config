a87c463237f5c99eb6d683e863dd48305f706b72
Kasper Therkelsen <kasper@kaspertherkelsen.com>
2025-03-23 12:33:57 +0100

    test

--- flake.nix
+++ flake.nix
@@ -10,16 +10,19 @@
   outputs =
     {
     nixpkgs,
-    nix-darwin
-    }:
+    nix-darwin,
+    ...
+    }@inputs:
     let
       lib = nixpkgs.lib;
+      pkgs = nixpkgs.legacyPackages.aarch64-darwin;
     in
     {
     nixosConfigurations = {
       AURA-M3GM7PN7F2 = nix-darwin.lib.darwinSystem {
         system = "aarch64-darwin";
         modules = [ ./hosts/AURA-M3GM7PN7F2/configuration.nix ];
+        specialArgs = { inherit inputs; };
       };
       t14 = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
