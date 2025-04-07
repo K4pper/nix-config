f85aca67fd2b0280dac557f3c5a68f752bc65a22
Kasper Therkelsen <kasper@kaspertherkelsen.com>
2025-04-07 08:59:55 +0200

    added hyprland config

--- modules/home-manager/desktop/hyprland/default.nix
+++ modules/home-manager/desktop/hyprland/default.nix
@@ -8,6 +8,21 @@
   programs.
   wayland.windowManager.hyprland.enable = true;
   wayland.windowManager.hyprland.settings = {
-
+    monitor = [
+      ",highrr,auto,auto"
+    ];
+    "$mainMod" = "$SUPER";
+    "$terminal" = "alacritty";
+    "$menu" = "wofi --show drun";
+    "$lock" = "hyprlock";
+    exec-once = [
+      "hyprpolkitagent"
+    ];
+    input = {
+      kb_layout = "us";
+      follow_mouse = 1;
+      sensitivity = -0.5;
+      accel_profile = "adaptive";
+    };
   };
 }
