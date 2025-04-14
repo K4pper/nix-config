c83b2db235b99406694312c4062155a66f64c4ee
Your Name <you@example.com>
2025-04-14 20:12:10 +0200

    added signingkey

--- hosts/t14/modules/home-manager/cli/git/default.nix
+++ hosts/t14/modules/home-manager/cli/git/default.nix
@@ -11,7 +11,7 @@
         rerere.enabled = true;
         delta.line-numbers = true;
         delta.side-by-side = true;
-        user.signingkey = "";
+        user.signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHD/4RuNDIN7KQhAHV4ZIZNnLe1sxS7ykMdS8xrcvykj";
         gpg.ssh.program = "${pkgs._1password-gui}/bin/op-ssh-sign";
       };
    };
