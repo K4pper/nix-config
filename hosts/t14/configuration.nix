6a425e5330678d471706cca9c338a2f25048220a
Kasper Therkelsen <kath@aura.dk>
2025-04-15 10:25:45 +0200

    took teams-for-linux out of firejail

--- hosts/t14/configuration.nix
+++ hosts/t14/configuration.nix
@@ -46,7 +46,6 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
--- hosts/t14/modules/home-manager/apps/teams/default.nix
+++ hosts/t14/modules/home-manager/apps/teams/default.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    teams
+    teams-for-linux
   ];
 }
