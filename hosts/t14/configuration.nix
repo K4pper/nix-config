54f28cebaddcabbd077f358d19de7353a75ab8ca
Kasper Therkelsen <kath@aura.dk>
2025-04-22 15:36:44 +0200

    enable A2DP

--- hosts/t14/configuration.nix
+++ hosts/t14/configuration.nix
@@ -39,6 +39,11 @@
   hardware.bluetooth = {
     enable = true;
     powerOnBoot = true;
+    settings = {
+      General = {
+        Enable = "Source,Sink,Media,Socket";
+      };
+    };
   };
   services.blueman.enable = true;
 
