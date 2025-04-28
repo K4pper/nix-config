238333c1ff6d3499febcc5755714d154411d8038
Kasper Therkelsen <kath@aura.dk>
2025-04-28 14:58:27 +0200

    added media keys

--- hosts/modules/home-manager/desktop/hyprland/default.nix
+++ hosts/modules/home-manager/desktop/hyprland/default.nix
@@ -84,6 +84,16 @@
       # Brightness
       ", XF86MonBrightnessUp, exec, brightnessctl set 5%+"
       ", XF86MonBrightnessDown, exec, brightnessctl set 5%-"
+
+      # Media keys
+      ", XF86AudioRaiseVolume, exec, pamixer -i 5"
+      ", XF86AudioLowerVolume, exec, pamixer -d 5"
+      ", XF86AudioMute, exec, pamixer -t"
+      ", XF86AudioMicMute, exec, pamixer --default-source -m"
+      ", XF86AudioPlay, exec, playerctl play-pause"
+      ", XF86AudioPause, exec, playerctl play-pause"
+      ", XF86AudioNext, exec, playerctl next"
+      ", XF86AudioPrev, exec, playerctl previous"
     ];
     xwayland = {
       force_zero_scaling = true;
