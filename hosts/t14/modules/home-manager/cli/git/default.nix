dec5ea58518142f968634773f759d76a8fb6090b
Kasper Therkelsen <kath@aura.dk>
2025-04-29 09:35:42 +0200

    changed signing key to be on host

--- hosts/t14/modules/home-manager/cli/git/default.nix
+++ hosts/t14/modules/home-manager/cli/git/default.nix
@@ -11,7 +11,7 @@
         rerere.enabled = true;
         delta.line-numbers = true;
         delta.side-by-side = true;
-        user.signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHD/4RuNDIN7KQhAHV4ZIZNnLe1sxS7ykMdS8xrcvykj";
+        user.signingkey = "~/.ssh/id_ed25519.pub";
       };
    };
 }
