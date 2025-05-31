e73f80436a8dd1d7fb93a073cbb6a57f2f760b4e
Kasper Therkelsen <kasper@kaspertherkelsen.com>
2025-05-31 07:59:11 +0200

    changed git config to use local ssh key

--- hosts/jupiter/modules/home-manager/cli/git/default.nix
+++ hosts/jupiter/modules/home-manager/cli/git/default.nix
@@ -11,10 +11,9 @@
         rerere.enabled = true;
         delta.line-numbers = true;
         delta.side-by-side = true;
-        user.signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIDLaZ/mOUAlAnC0+I1BydvDE5x9pcXPZW7U9RPz3nn4";
-        gpg.ssh.program = "${pkgs._1password-gui}/bin/op-ssh-sign";
+        user.signingkey = "~/.ssh/id_ed25519.pub";
       };
-   };
+    };
 }
 
 
