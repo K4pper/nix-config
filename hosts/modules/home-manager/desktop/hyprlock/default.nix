10ffb275f6f040c179b60d9706077daa51eba3b5
Kasper Therkelsen <kasper@kaspertherkelsen.com>
2025-05-31 09:35:28 +0200

    hyprlock config

--- hosts/modules/home-manager/desktop/hyprlock/default.nix
+++ hosts/modules/home-manager/desktop/hyprlock/default.nix
@@ -1,6 +1,6 @@
 { ... }:
 {
-  home.file.".config/hyprlock/hyprlock.png" = {
+  home.file.".config/hypr/hyprlock.png" = {
     source = ./hyprlock.png;
     executable = false;
   };
@@ -8,17 +8,14 @@
     enable = true;
     settings = {
       general = {
-        no_fade_in = true;
-        no_fade_out = true;
+        no_fade_in = false;
         grace = 0;
-        disable_loading_bar = true;
-        hide_cursor = false;
+        disable_loading_bar = false;
       };
       background = [
       {
-        path = "screenshot";
+        path = "~/.config/hypr/hyprlock.png";
         blur_passes = 2;
-        blur_size = 7;
         contrast = 0.8916;
         brightness = 0.8172;
         vibrancy = 0.1696;
@@ -30,30 +27,39 @@
         size = "250, 60";
         outline_thickness = 2;
         dots_size = 0.2;
-        dots_spacing = 0.35;
+        dots_spacing = 0.2;
         dots_center = true;
         outer_color = "rgba(0, 0, 0, 0)";
-        inner_color = "rgba(0, 0, 0, 0.2)";
-        font_color = "rgba(255, 255, 255, 255)";
+        inner_color = "rgba(100, 114, 125, 0.4)";
+        font_color = "rgba(200, 200, 200)";
         fade_on_empty = false;
-        rounding = -1;
-        check_color = "rgb(204, 136, 34)";
         placeholder_text = "<i><span foreground='##cdd6f4'>Input Password... </span></i>";
         hide_input = false;
-        position = "0, -200";
+        position = "0, -150";
         halign = "center";
         valign = "center";
       }
       ];
       label = [
+        # TIME
         {
           text = "$TIME";
           color = "rgba(242, 243, 244, 0.75)";
           font_size = 95;
           font_family = "JetBrains Mono Extrabold";
-          position = "-30, -200";
+          position = "0, -370";
           halign = "center";
           valign = "top";
+        }
+        # DATE
+        {
+          text = ''cmd[update:1000] echo -e "$(date +"%A, %d %B")"'';
+          color = "rgba(216, 222, 233, 0.70)";
+          font_size = 30;
+          font_family = "JetBrains Mono Extrabold";
+          position = "0, 170";
+          halign = "center";
+          valign = "center";
         }
       ];
     };
