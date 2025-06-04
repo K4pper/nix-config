c1d9b3fa8e11347fac4560fd4b65fe56d0e65c66
Kasper Therkelsen <kath@aura.dk>
2025-06-04 12:07:34 +0200

    added fonts and added bluetooth module to waybar

--- hosts/modules/home-manager/desktop/fonts/default.nix
+++ hosts/modules/home-manager/desktop/fonts/default.nix
@@ -3,5 +3,12 @@
   home.packages = with pkgs; [
     google-fonts
     nerd-fonts.jetbrains-mono
+    noto-fonts
+    noto-fonts-cjk-sans
+    noto-fonts-emoji
+    liberation_ttf
+    fira-code
+    fira-code-symbols
+    dina-font
   ];
 }
