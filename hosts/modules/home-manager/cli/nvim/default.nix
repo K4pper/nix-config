31e90770016cc4282577761093d38b63d7a3125f
Kasper Therkelsen <kath@aura.dk>
2025-07-22 09:32:17 +0200

    start of neovim conf

--- hosts/modules/home-manager/cli/nvim/default.nix
+++ hosts/modules/home-manager/cli/nvim/default.nix
@@ -4,5 +4,16 @@
     enable = true;
     viAlias = true;
     vimAlias = true;
+    vimdiffAlias = true;
+    defaultEditor = true;
+    extraLuaConfig = ''
+      ${builtins.readFile ./settings.lua}
+    '';
+    plugins = with pkgs.vimPlugins; [
+      {
+        plugin = rose-pine;
+        config = "colorscheme rose-pine";
+      }
+    ];
   };
 }
