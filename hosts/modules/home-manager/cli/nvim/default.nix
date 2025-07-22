6338d01097b2efa62e2de9326e8b2fc93aae3da0
Kasper Therkelsen <kath@aura.dk>
2025-07-22 09:04:31 +0200

    changed from nixvim to neovim

--- hosts/modules/home-manager/cli/nvim/default.nix
+++ hosts/modules/home-manager/cli/nvim/default.nix
@@ -1,22 +1,8 @@
-{...}:
+{ ... }:
 {
-  imports = [
-    ./options.nix
-    ./plugins
-    ./keymappings.nix
-    ./autocommands.nix
-  ];
-  programs.nixvim = {
+  programs.neovim = {
     enable = true;
-    defaultEditor = true;
-    vimdiffAlias = true;
     viAlias = true;
     vimAlias = true;
-    extraConfigLua = ''
-      local bicep_lsp_bin = "/usr/local/bin/bicep-langserver/Bicep.LangServer.dll"
-      require'lspconfig'.bicep.setup{
-      cmd = { "dotnet", bicep_lsp_bin };
-    }
-    '';
   };
 }
