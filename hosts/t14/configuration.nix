79831fcb557a93b3392f228343492cb6c5f43c26
Kasper Therkelsen <kath@aura.dk>
2025-07-29 12:12:09 +0200

    remove TLP config

--- hosts/t14/configuration.nix
+++ hosts/t14/configuration.nix
@@ -61,27 +61,6 @@
       size = 32*1024;
     }
   ];
-  services.tlp = {
-    enable = true;
-    settings = {
-      CPU_SCALING_GOVERNOR_ON_AC = "performance";
-      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-
-      CPU_MIN_PERF_ON_AC = 40;
-      CPU_MAX_PERF_ON_AC = 100;
-      CPU_MIN_PERF_ON_BAT = 40;
-      CPU_MAX_PERF_ON_BAT = 50;
-
-       #Optional helps save long term battery health
-       START_CHARGE_THRESH_BAT0 = 40; # 40 and below it starts to charge
-       STOP_CHARGE_THRESH_BAT0 = 95; # 80 and above it stops charging
-
-    };
-  };
-
   services.keyd = {
     enable = true;
     keyboards.default.settings = {
